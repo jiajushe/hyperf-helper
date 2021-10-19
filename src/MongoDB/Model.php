e067f1c311be1706a5759ee3a8ea0fd2cc55e302
SmartYuan <smart.yuan@hotmail.com>
2021-10-19 15:20:21 +0800

    MongoDB Task

--- src/MongoDB/Model.php
+++ src/MongoDB/Model.php
@@ -210,7 +210,7 @@ class Model
     final public function execute(...$param)
     {
         $method = array_shift($param);
-        return $this->collectionObj->$method(...$param)->toArray();
+        return $this->collectionObj->$method(...$param);
 //        $parallel = new Parallel($this->config['concurrent']);
 //        $parallel->add(function () use ($param) {
 //            return $this->collectionObj->$method(...$param);
