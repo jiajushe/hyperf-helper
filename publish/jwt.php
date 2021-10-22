82970fd0d731b4142185840efa83c5c3868a469c
SmartYuan <smart.yuan@hotmail.com>
2021-10-22 10:27:03 +0800

    MongoDB Task

--- publish/jwt.php
+++ publish/jwt.php
@@ -4,7 +4,7 @@
  */
 return [
     'secret' => env('JWT_SECRET', '3dfa538edae7c1021a234afe9c7906c3'),
-    'expire_second' => env('JWT_EXPIRE_SECOND', 7200),
-    'refresh_second' => env('JWT_REFRESH_SECOND', 1209600),
+    'expire_second' => env('JWT_EXPIRE_SECOND', 72000),
+    'refresh_second' => env('JWT_REFRESH_SECOND', 1200),
     'leeway_second' => 60,
 ];
--- src/JWT/Token.php
+++ src/JWT/Token.php
@@ -9,30 +9,30 @@ use Firebase\JWT\SignatureInvalidException;
 use InvalidArgumentException;
 use Jiajushe\HyperfHelper\Exception\CustomNormal;
 use Throwable;
+use stdClass;
 use UnexpectedValueException;
 
 class Token
 {
     /**
      * @param string $model
-     * @param array $user
+     * @param stdClass $user
      * @return string
      * @throws Throwable
      */
-    public function make(string $model, array $user): string
+    public function make(string $model, stdClass $user): string
     {
         try {
             $config = config('jwt');
             $time = time();
-            $sub = $user['id'];
-            unset($user['id']);
+            $sub = $user->id;
+            unset($user->id);
             $payload = [
                 'iss' => $model,    //签发者
                 'sub' => $sub, //用户ID
-                'exp' => $time + $config['refresh_second'], //过期时间
+                'exp' => $time + $config['expire_second'], //过期时间
                 'nbf' => $time, //某个时间点后才能访问
                 'iat' => $time, //签发时间
-                'refresh' => $time + $config['expire_second'],  //刷新时间
                 'info' => $user,
             ];
             return JWT::encode($payload, $config['secret']);
@@ -53,7 +53,9 @@ class Token
             $config = config('jwt');
             JWT::$leeway = $config['leeway_second'];
             $payload = JWT::decode($user_token, $config['secret'], ['HS256']);
-            pp($payload);
+            if ($payload->iss != $model) {
+                throw new SignatureInvalidException();
+            }
             return $payload;
         } catch (InvalidArgumentException $e) {
             throw new CustomNormal('没有签名', config('res_code.token'));
