44668c125126e3059e12d801198acf6294659e5d
yun <smart.yuan@hotmail.com>
2021-10-28 14:26:48 +0800

    Model.php

--- src/MongoDB/Model.php
+++ src/MongoDB/Model.php
@@ -307,13 +307,13 @@ abstract class Model
                 if (!is_array($value)) {
                     throw new CustomError('$value must be array');
                 }
-                $this->filter['$and'][$field] = [self::OPERATORS['between'][0] => $value[0], self::OPERATORS['between'][1] => $value[1]];
+                $this->filter['$and'][][$field] = [self::OPERATORS['between'][0] => $value[0], self::OPERATORS['between'][1] => $value[1]];
                 return $this;
             }
             if ($operator == 'in' && !is_array($value)) {
                 throw new CustomError('$value must be array');
             }
-            $this->filter['$and'][$field] = [self::OPERATORS[$operator] => $value];
+            $this->filter['$and'][][$field] = [self::OPERATORS[$operator] => $value];
         }
         return $this;
     }
@@ -326,7 +326,7 @@ abstract class Model
     final public function exists(array $field_arr): Model
     {
         foreach ($field_arr as $index => $item) {
-            $this->filter['$and'][$this->idTo_id($this->idTo_id($index))] = ['$exists' => $item];
+            $this->filter['$and'][][$this->idTo_id($this->idTo_id($index))] = ['$exists' => $item];
         }
         return $this;
     }
