343e84f2339422764e98259cfa8a585b4f8cdb2f
SmartYuan <smart.yuan@hotmail.com>
2021-11-02 15:19:36 +0800

    GuzzleHelper

---
+++ src/Helper/GuzzleHelper.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Jiajushe.
+ *
+ * @link
+ * @document
+ * @contact
+ * @license
+ */
+namespace Jiajushe\HyperfHelper\Helper;
+
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Guzzle\ClientFactory;
+use Jiajushe\HyperfHelper\Exception\CustomError;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+class GuzzleHelper
+{
+    /**
+     * @Inject
+     */
+    protected ClientFactory $clientFactory;
+
+    /**
+     * @param string $method 请求方法
+     * @param string $uri 请求地址
+     * @param array $query 请求参数
+     * @param array $options 请求选项
+     * @param int $concurrent 最大同时运行的协程数
+     * @throws CustomError
+     */
+    public function request(string $method, string $uri, array $query, array $options = [], int $concurrent = 0): ResponseInterface
+    {
+        $options['query'] = $query;
+        $res = parallel([
+            function () use ($method, $uri, $options) {
+                try {
+                    return $this->clientFactory->create()->request($method, $uri, $options);
+                } catch (Throwable $t) {
+                    $this->errorLog($t, [$method, $uri, $options]);
+                    return null;
+                }
+            },
+        ], $concurrent);
+        if ($res[0] === null) {
+            throw new CustomError('weixin connect error');
+        }
+        return $res[0];
+    }
+
+    /**
+     * 错误记录.
+     * @param Throwable $throwable
+     * @param array $data 请求数据
+     */
+    private function errorLog(Throwable $throwable, array $data)
+    {
+//        todo http请求错误记录
+        pp(
+            $data,
+            $throwable->getMessage(),
+            $throwable->getCode(),
+            $throwable->getLine(),
+            $throwable->getFile(),
+            $throwable->getTrace()
+        );
+    }
+}
