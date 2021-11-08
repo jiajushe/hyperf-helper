a72310a39379683df605c187edfe767c7dd788d1
SmartYuan <smart.yuan@hotmail.com>
2021-11-08 18:33:53 +0800

    services.php

--- publish/services.php
+++ publish/services.php
@@ -13,7 +13,7 @@ return [
     'consumers' => [
         [
             // name 需与服务提供者的 name 属性相同
-            'name' => 'AdminTokenJsonRPCInterface',
+            'name' => 'AdminTokenJsonRPC',
             // 服务接口名，可选，默认值等于 name 配置的值，如果 name 直接定义为接口类则可忽略此行配置，如 name 为字符串则需要配置 service 对应到接口类
             'service' => Jiajushe\HyperfHelper\JsonRPCInterface\AdminTokenJsonRPCInterface::class,
             // 服务提供者的服务协议，可选，默认值为 jsonrpc-http
