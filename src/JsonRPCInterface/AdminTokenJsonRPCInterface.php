4583ab06c5f8d51a5ba25718866b572678b5370d
SmartYuan <smart.yuan@hotmail.com>
2021-11-08 10:01:51 +0800

    TokenJsonRPCInterface

--- src/JsonRPCInterface/AdminTokenJsonRPCInterface.php
+++ src/JsonRPCInterface/AdminTokenJsonRPCInterface.php
@@ -3,5 +3,19 @@ namespace Jiajushe\HyperfHelper\JsonRPCInterface;
 
 interface AdminTokenJsonRPCInterface
 {
+    /**
+     * admin token 验证.
+     * @param string $token
+     * @return mixed
+     */
     public function verify(string $token);
+
+    /**
+     * admin permission 权限验证.
+     * @param string $token
+     * @param string $method
+     * @param string $path
+     * @return mixed
+     */
+    public function permission(string $token, string $method, string $path);
 }
--- src/JsonRPCInterface/CustomerTokenJsonRPCInterface.php
+++ src/JsonRPCInterface/CustomerTokenJsonRPCInterface.php
@@ -3,5 +3,19 @@ namespace Jiajushe\HyperfHelper\JsonRPCInterface;
 
 interface CustomerTokenJsonRPCInterface
 {
+    /**
+     * customer token 验证.
+     * @param string $token
+     * @return mixed
+     */
     public function verify(string $token);
+
+    /**
+     * customer permission 权限验证.
+     * @param string $token
+     * @param string $method
+     * @param string $path
+     * @return mixed
+     */
+    public function permission(string $token, string $method, string $path);
 }
