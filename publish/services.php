ceaf14e02bfa7e83e07bd5e99957b22c3c9bc4d9
SmartYuan <smart.yuan@hotmail.com>
2021-11-08 14:16:34 +0800

    services.php

--- publish/services.php
+++ publish/services.php
@@ -29,5 +29,23 @@ return [
                 ],
             ],
         ],
+        [
+            // name 需与服务提供者的 name 属性相同
+            'name' => 'CustomerTokenJsonRPC',
+            // 服务接口名，可选，默认值等于 name 配置的值，如果 name 直接定义为接口类则可忽略此行配置，如 name 为字符串则需要配置 service 对应到接口类
+            'service' => Jiajushe\HyperfHelper\JsonRPCInterface\CustomerTokenJsonRPCInterface::class,
+            // 服务提供者的服务协议，可选，默认值为 jsonrpc-http
+            // 可选 jsonrpc-http jsonrpc jsonrpc-tcp-length-check
+            'protocol' => 'jsonrpc-http',
+            // 负载均衡算法，可选，默认值为 random
+            'load_balancer' => 'random',
+            // 如果没有指定上面的 registry 配置，即为直接对指定的节点进行消费，通过下面的 nodes 参数来配置服务提供者的节点信息
+            'nodes' => [
+                [
+                    'host' => env('JSONRPC_SERVICE_TOKEN_HOST', 'saas-auth'),
+                    'port' => (int) env('JSONRPC_SERVICE_TOKEN_PORT', 9502),
+                ],
+            ],
+        ],
     ],
 ];
