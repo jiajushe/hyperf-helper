a638675e484e2a33749583d259a617b4ef314535
SmartYuan <smart.yuan@hotmail.com>
2021-11-18 11:02:28 +0800

    WeixinErrorLog.php

--- src/MongoDB/WeixinErrorLog.php
+++ src/MongoDB/WeixinErrorLog.php
@@ -4,5 +4,14 @@ namespace Jiajushe\HyperfHelper\MongoDB;
 
 class GuzzleErrorLog extends Model
 {
-
+    public function log(string $appid, $method, string $uri, array $query, array $response)
+    {
+        $this->create([
+            'appid' => $appid,
+            'method' => $method,
+            'uri' => $uri,
+            'query' => $query,
+            'response' => $response,
+        ]);
+    }
 }
